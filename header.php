1c5af242639a99ee35f86823cf9757d68ca96936
atelierabbey <info@sillycoyote.com>
2013-10-23 13:24:27 -0700

    Optimized Header.php
    According to https://developers.google.com/speed/articles/optimizing-html removed optional tags.

--- header.php
+++ header.php
@@ -1,25 +1,23 @@
-<!DOCTYPE html>
-<html class="<?php if (function_exists('css_browser_selector')){echo css_browser_selector().' ';} if (function_exists('skinfo')){ echo skinfo('Version'); }?>"  dir="ltr" lang="en-US">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" type="image/png" href="<?php bloginfo('stylesheet_directory');?>/img/favicon.png?v=1" />
-    <link rel="alternate" type="application/rss+xml" title="RSS 2.0 Feed" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/func.css?ver=1" type="text/css" />
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/style.css?ver=1" type="text/css" />
-	<?php wp_enqueue_script("jquery"); ?>
-    <!--[if lt IE 9]> <script src="<?php bloginfo('template_directory'); ?>/js/html5.js"></script> <![endif]-->
-	<?php wp_head(); ?>
-</head>
-<body id="post-<?php the_ID(); ?>" <?php body_class(); ?>>
+<!DOCTYPE html><html class="<?php if (function_exists('css_browser_selector')){echo css_browser_selector().' ';} if (function_exists('skinfo')){ echo skinfo('Version'); }?>"  dir="ltr" lang="en-US"><meta charset=utf-8><title><?php wp_title( '|', true, 'right' ); ?></title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><link rel="shortcut icon" type="image/png" href="<?php bloginfo('stylesheet_directory');?>/img/favicon.png?v=1" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0 Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/func.css?ver=1" type="text/css" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/style.css?ver=1" type="text/css" />
+<!--[if lt IE 9]><script src="<?php bloginfo('template_directory'); ?>/js/html5.js"></script><![endif]-->
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php wp_enqueue_script("jquery"); ?>
+<?php wp_head(); ?>
+<body id="page-<?php the_ID(); ?>" <?php body_class(); ?>>
 <div id="preloader"></div>
 <div class="wrapper">
-    <header>
-        <div class="logo"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="Logo"></a></div>
-        <nav class="access">
-            <?php $menuargs = array(
+   <header>
+	<div class="logo">
+		<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="Logo">
+		</a>
+	</div>
+	<nav class="access">
+            <?php wp_nav_menu( array(
                 'theme_location'  => 'main',
                 'menu'            => '',
                 'container'       => false,
@@ -36,7 +34,7 @@
                 'items_wrap'      => '<ul>%3$s</ul>',
                 'depth'           => 3, // 0 = all. Default, -1 = displays links at any depth and arranges them in a single, flat list.
                 'walker'          => ''
-            ); wp_nav_menu( $menuargs ); ?>
+            )); ?>
         </nav>
     </header>
     <div class="clear"></div>
