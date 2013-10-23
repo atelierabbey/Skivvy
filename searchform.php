d07db663a8b0fab67f7b8425178f3bfa2db74e01
atelierabbey <info@sillycoyote.com>
2013-10-23 16:30:59 -0700

    added default hidden text visible by screen readers

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form class="search-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 	<label>
-		<span class="screen-reader-text">Search for:</span>
+		<span class="screenreader">Search for:</span>
 		<input class="search-field" type="search" placeholder="Search …" value="" name="s" title="Search for:" />
 	</label>
 	<input class="search-submit" type="submit" value="Search" />
