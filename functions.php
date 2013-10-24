6df1d62011dd47fb5e2652e5c788291646b7ea63
atelierabbey <info@sillycoyote.com>
2013-10-24 12:20:18 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,12 @@
-<?php #23Oct13
-add_action( 'after_setup_theme', 'skivvy_setup' );
-function skivvy_setup() {
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
-	# add_theme_support( 'post-formats', array('aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video') );
-	# add_filter('widget_text', 'do_shortcode'); // Widget Support - Shortcodes 
-	# add_filter('widget_text', array( $wp_embed, 'run_shortcode' ), 8 );
-	# add_filter('widget_text', array( $wp_embed, 'autoembed'), 8 ); // Widget Support - oEmbed & oEmbed 2
-	# if (function_exists('skivvy_autooptions')){skivvy_autooptions();} // Auto setup options on theme setup - In lib/wpsimple.php
-	/* 	load_theme_textdomain( 'skivvy', TEMPLATEPATH.'/lng' );
-		$locale = get_locale();
-		$locale_file = TEMPLATEPATH."/lng/$locale.php";
-		if ( is_readable( $locale_file ) )require_once( $locale_file ); //*/
+<?php add_action( 'after_setup_theme', 'skivvy_setup' ); function skivvy_setup() {
+  add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+# add_theme_support( 'post-formats', array('aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video') );
+# add_filter('widget_text', 'do_shortcode'); // Widget Support - Shortcodes 
+# add_filter('widget_text', array( $wp_embed, 'run_shortcode' ), 8 );
+# add_filter('widget_text', array( $wp_embed, 'autoembed'), 8 ); // Widget Support - oEmbed & oEmbed 2
+# if (function_exists('skivvy_autooptions')){skivvy_autooptions();} // Auto setup options on theme setup - In lib/wpsimple.php
+# load_theme_textdomain( 'skivvy', TEMPLATEPATH.'/lng' ); $locale_file = TEMPLATEPATH.'/lng/'.get_locale().'.php'; if ( is_readable($locale_file)){require_once( $locale_file );}
 } 
 
 // Unique Registers
@@ -19,17 +14,17 @@ function skivvy_setup() {
 # include_once 'inc/register_menu.php';
 # include_once 'inc/register_posttype.php';
 # include_once 'inc/register_scripts.php';
-
-// Progressive Registers
 # include_once 'inc/register_widget.php';
+// Progressive Registers
 # include_once 'inc/register_shortcode.php';
 
 // Modules
-# include_once 'lib/skivvy_branding.php';
-# include_once 'lib/wpsimple.php';
+include_once 'lib/skivvy_branding.php';
+include_once 'lib/wpsimple.php';
 # include_once 'lib/cms_client.php';
 # include_once 'lib/cms_admin.php';
 # include_once 'lib/cms_weboptions.php'; 
+# include_once 'lib/mod_bucket.php';
 # include_once 'lib/mod_browserdetect.php';
 # include_once 'lib/mod_randimg.php'; 
 # include_once 'lib/mod_advstyles.php'; 
