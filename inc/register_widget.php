759c255d79afaf5ebb27b020e7b44cc0ac3ea5c3
atelierabbey <info@sillycoyote.com>
2013-10-24 12:10:56 -0700

    Cleaned register_widget.php

--- inc/register_widget.php
+++ inc/register_widget.php
@@ -1,7 +1,3 @@
-<?php #22Aug13
-function skivvy_add_widget() {
-	register_widget( 'Bucket_Widget' );
-} add_action( 'widgets_init', 'skivvy_add_widget' );
-
-include_once('widget_bucket.php');
-?>
+<?php function skivvy_add_widget() {
+	
+} add_action( 'widgets_init', 'skivvy_add_widget' ); ?>
