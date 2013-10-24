5d5d81204331a7d82477d2e49b283077e876137d
atelierabbey <info@sillycoyote.com>
2013-10-24 12:28:17 -0700

    Update cms_admin.php

--- lib/cms_admin.php
+++ lib/cms_admin.php
@@ -7,24 +7,24 @@ function skivvy_admin_bar() {
 	global $wp_admin_bar;
 	//// ---- Remove stuff ---- ////
 
-    $wp_admin_bar->remove_menu('wp-logo');          // Remove the WordPress logo
-    $wp_admin_bar->remove_menu('about');            // Remove the about WordPress link
-    $wp_admin_bar->remove_menu('wporg');            // Remove the WordPress.org link
-    $wp_admin_bar->remove_menu('documentation');    // Remove the WordPress documentation link
-    $wp_admin_bar->remove_menu('support-forums');   // Remove the support forums link
-    $wp_admin_bar->remove_menu('feedback');         // Remove the feedback link
-    //$wp_admin_bar->remove_menu('site-name');        // Remove the site name menu
-    $wp_admin_bar->remove_menu('view-site');        // Remove the view site link
-    $wp_admin_bar->remove_menu('updates');          // Remove the updates link
-    $wp_admin_bar->remove_menu('comments');         // Remove the comments link
-    $wp_admin_bar->remove_menu('w3tc');             // If you use w3 total cache remove the performance link
-    $wp_admin_bar->remove_menu('my-account');       // Remove the user details tab
+	$wp_admin_bar->remove_menu('wp-logo');          // Remove the WordPress logo
+	$wp_admin_bar->remove_menu('about');            // Remove the about WordPress link
+	$wp_admin_bar->remove_menu('wporg');            // Remove the WordPress.org link
+	$wp_admin_bar->remove_menu('documentation');    // Remove the WordPress documentation link
+	$wp_admin_bar->remove_menu('support-forums');   // Remove the support forums link
+	$wp_admin_bar->remove_menu('feedback');         // Remove the feedback link
+	//$wp_admin_bar->remove_menu('site-name');        // Remove the site name menu
+	$wp_admin_bar->remove_menu('view-site');        // Remove the view site link
+	$wp_admin_bar->remove_menu('updates');          // Remove the updates link
+	$wp_admin_bar->remove_menu('comments');         // Remove the comments link
+	$wp_admin_bar->remove_menu('w3tc');             // If you use w3 total cache remove the performance link
+	$wp_admin_bar->remove_menu('my-account');       // Remove the user details tab
 	$wp_admin_bar->remove_menu('new-content');		// Remove the content link
-    $wp_admin_bar->remove_menu('new-media');
-    $wp_admin_bar->remove_menu('new-link'); 
-    $wp_admin_bar->remove_menu('new-user');
-    $wp_admin_bar->remove_menu('new-theme'); 
-    $wp_admin_bar->remove_menu('new-plugin');
+	$wp_admin_bar->remove_menu('new-media');
+	$wp_admin_bar->remove_menu('new-link'); 
+	$wp_admin_bar->remove_menu('new-user');
+	$wp_admin_bar->remove_menu('new-theme'); 
+	$wp_admin_bar->remove_menu('new-plugin');
 	$wp_admin_bar->remove_menu('dashboard'); 
 	$wp_admin_bar->remove_menu('themes');
 	$wp_admin_bar->remove_menu('customize');
