db2ddacdf62c41e0615e5330020844618db6ca0b
atelierabbey <info@sillycoyote.com>
2013-11-14 08:49:58 -0800

    Added cheatsheet link

--- inc/register_posttype.php
+++ inc/register_posttype.php
@@ -1,4 +1,4 @@
-<?php #8Aug13 - http://generatewp.com
+<?php #8Aug13 - http://generatewp.com or http://justintadlock.com/archives/2013/09/13/register-post-type-cheat-sheet
 function register_skivvy_posttype() { 
 
 } add_action( 'init', 'register_skivvy_posttype' );?>
