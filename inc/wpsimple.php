cb783a2836c6ffbad6ae5e98c9eb6cc4cc862847
atelierabbey <info@sillycoyote.com>
2014-01-24 11:07:03 -0800

    Moved AutoOptions to its own file, cleaned up functions.php

--- inc/wpsimple.php
+++ inc/wpsimple.php
@@ -1,4 +1,4 @@
-<?php // 5Jan14
+<?php // 24Jan14
 // remove wp_head fluff
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
@@ -82,44 +82,11 @@ function skivvy_admin_bar() {
 	
 } add_action('wp_before_admin_bar_render', 'skivvy_admin_bar', 0);
 
-function skivvy_autooptions() {
-	$the_theme_status = get_option( 'theme_setup_status' );
-	if ( $the_theme_status !== '1' ) {
-		// Setup Default WordPress settings
-		$core_settings = array(
-			'show_avatars' => false,
-			'avatar_default' => 'mystery',
-			'avatar_rating' => 'G',
-			'default_role' => 'editor',
-			'comments_per_page' => 20,
-			'uploads_use_yearmonth_folders' => false,
-		);
-		foreach ( $core_settings as $k => $v ) {update_option( $k, $v );}
-
-		// Delete dummy post, page and comment.
-		wp_delete_post( 1, true );
-		wp_delete_post( 2, true );
-		wp_delete_comment( 1 );
-
-		update_option( 'theme_setup_status', '1' );
-		$msg = '<div class="error"><p>The '.get_option( 'current_theme' ).' theme has changed your WordPress default <a href="' . admin_url( 'options-general.php' ) . '" title="See Settings">settings</a> and deleted default posts & comments.</p></div>';
-		add_action( 'admin_notices', $c = create_function( '', 'echo "' . addcslashes( $msg, '"' ) . '";' ) );
-	}
-	// Else if we are re-activing the theme
-	elseif ( $the_theme_status === '1' and isset( $_GET['activated'] ) ) {
-		$msg = '
-		<div class="updated">
-			<p>The ' . get_option( 'current_theme' ) . ' theme was successfully re-activated.</p>
-		</div>';
-		add_action( 'admin_notices', $c = create_function( '', 'echo "' . addcslashes( $msg, '"' ) . '";' ) );
-	}
-} 
-
-
 // Removes "Private" & "Protected" from titles
 add_filter( 'private_title_format',   'remove_the_title_stuff' );
 add_filter( 'protected_title_format', 'remove_the_title_stuff' );
 function remove_the_title_stuff( $title ) {
 	return '%s';
-}
+} 
+
 ?>
