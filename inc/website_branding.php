15f912434de366bbbc91575e4b8d4ff0b2328417
atelierabbey <info@sillycoyote.com>
2014-01-24 11:12:56 -0800

    Unminified Wp-branding, made more readable

--- inc/website_branding.php
+++ inc/website_branding.php
@@ -1,4 +1,4 @@
-<?php #17Oct13 - Bat Branding
+<?php #24Jan14
 
 //// ---- skinfo('$what'); Get Version from style.css ---- ////
 function skinfo($what='Version') {
@@ -18,7 +18,12 @@ function skinfo($what='Version') {
 	}
 }
 
-//// ---- theme branding & dashboard widgets
+/*
+ *
+ *		WP-Admin branding
+ *
+ */
+ 
 function skivvy_footer_admin() {
 	echo 'Fueled by <a href="http://www.wordpress.org" target="_blank">WordPress</a> | Design by <a href="'.skinfo("AuthorURI").'" target="_blank">'.skinfo('Author').'</a>';
 } add_filter('admin_footer_text', 'skivvy_footer_admin');
@@ -28,33 +33,43 @@ function skivvy_footer_version(){
 } add_filter( 'update_footer', 'skivvy_footer_version', 11 );
 
 function skivvy_admin_css() {
-	wp_enqueue_style('skivvy_admin_css',get_template_directory_uri().'/css/admin.css','','');
-} add_action('admin_print_styles','skivvy_admin_css'); add_action('login_head', 'skivvy_admin_css');
+	wp_enqueue_style( 'skivvy_admin_css', get_template_directory_uri().'/css/admin.css' , '' , '' );
+} add_action('admin_print_styles','skivvy_admin_css'); add_action( 'login_head', 'skivvy_admin_css' );
 
-function skivvy_admin_notice(){  // Add a warning box to the settings page Uses the same style box as the WordPress Update "update-nag"
+function skivvy_admin_notice(){ 
+	// Adds a warning box to the settings page Uses the same style box as the WordPress Update "update-nag"
 	global $current_screen;
 	if ( $current_screen->parent_base == 'options-general' ){
 		echo '<div id="admin-settings-warning-box"><strong>Warning</strong> - changing settings on these pages may cause problems with your website&rsquo;s design!</div>';
 	}
 } add_action('admin_notices', 'skivvy_admin_notice');
-/* // Adds custom message if Wordpress is out of date
+
+
+/* 
+// Adds custom message if Wordpress is out of date
 if (!current_user_can('edit_users')) {
 	add_action('init', create_function('$a', "remove_action('init', 'wp_version_check');"), 2);
 	add_filter('pre_option_update_core', create_function('$a', "return null;"));
 } // */
 
-//// ---- Custom WP-Admin Dashboard Widgets
-function skivvy_dashboard_help() {
-	echo '<p>Welcome to '.get_bloginfo( "name", "display" )."'s CMS! Need help? Contact <a href='".skinfo("AuthorURI")."' target='_blank'>".skinfo('Author')."</a>.</p>";
-}
-function skivvy_dashboard_feed() {
-	echo '<div class="rss-widget">';wp_widget_rss_output(array('url' => skinfo("AuthorURI").'feed/','title' => 'Latest News','items' => 5, 'show_summary' => 1,'show_author' => 0,'show_date' => 1));echo "</div>";
-}
 
-// Registers those widgets
-function skivvy_dashboard_widgets() {
-	wp_add_dashboard_widget('dashboard_custom_feed', skinfo('Author').' News Feed', 'skivvy_dashboard_feed');
-	wp_add_dashboard_widget('dashboard_custom_help', 'Website Technical Support', 'skivvy_dashboard_help');
-} add_action('wp_dashboard_setup', 'skivvy_dashboard_widgets');
+
+/*
+ *
+ *		Custom WP-Admin Dashboard Widgets
+ *
+ */
+	function skivvy_dashboard_help() {
+		echo '<p>Welcome to '.get_bloginfo( "name", "display" )."'s CMS! Need help? Contact <a href='".skinfo("AuthorURI")."' target='_blank'>".skinfo('Author')."</a>.</p>";
+	}
+	function skivvy_dashboard_feed() {
+		echo '<div class="rss-widget">';wp_widget_rss_output(array('url' => skinfo("AuthorURI").'feed/','title' => 'Latest News','items' => 5, 'show_summary' => 1,'show_author' => 0,'show_date' => 1));echo "</div>";
+	}
+
+	// Registers those widgets
+	function skivvy_dashboard_widgets() {
+		wp_add_dashboard_widget('dashboard_custom_feed', skinfo('Author').' News Feed', 'skivvy_dashboard_feed');
+		wp_add_dashboard_widget('dashboard_custom_help', 'Website Technical Support', 'skivvy_dashboard_help');
+	} add_action('wp_dashboard_setup', 'skivvy_dashboard_widgets');
 
 ?>
