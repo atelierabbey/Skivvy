13c7faf3e92a7c0c74ee3c593f509f39ab6b18c6
atelierabbey <info@sillycoyote.com>
2014-02-10 13:09:00 -0800

    Converted PHP-centric Slide-chunk

--- inc/chunk-slider.php
+++ inc/chunk-slider.php
@@ -1,23 +1,60 @@
-<?php wp_enqueue_script('jcycle2'); ?>
-<?php $slidequery = new WP_Query(array(
-	'post_type' => 'jCycle_slider',
-	'posts_per_page' => 5,
-	'order' => 'ASC',
-	'orderby' => 'none'
-)); ?>
-<div class="cycle-slideshow" style="position:relative;" data-cycle-pager="#per-slide-template" data-cycle-slides=".cycle-slide" >
-		<?php while ( $slidequery->have_posts() ) : $slidequery->the_post();
+<?php
+
+	// Enqueue Cycle 2 script - Wp-jcycle required https://github.com/atelierabbey/WPJcycle
+	// http://jquery.malsup.com/cycle2/api/
+	wp_enqueue_script('jcycle2');
+
+	// Start SLIDE WRAP
+		echo ( 
+			'<div '.
+				' class="cycle-slideshow"'. // Initializing class, default for cycle2
+				' data-cycle-pager="#per-slide-template"'. // Connects to #per-slide-template box below
+				' data-cycle-slides=".cycle-slide"'. // identifies the class identifier for slides
+				' data-pause-on-hover="true"'. // Pause on hover
+				' style="position:relative;"'.
+			'>'
+		);
+
+
+		// SLIDE LOOP
+			$slidequery = new WP_Query(array(
+				'post_type' => 'jCycle_slider',
+				'posts_per_page' => 5,
+				'order' => 'ASC',
+				'orderby' => 'none'
+			));
+			while ( $slidequery->have_posts() ) { $slidequery->the_post();
+
 				$image_id = get_post_thumbnail_id();
 				$image_url = wp_get_attachment_image_src($image_id,'full', true);
-		?><div class="cycle-slide slide-<?php the_ID();?>" style="position:absolute;<?php echo "background-image:url('{$image_url[0]}'); ')";?>">
-			<?php // echo '<h1>' . get_the_title() . '</h1>';?>
-			<?php //the_post_thumbnail();?>
-			<?php the_content(); ?>
-		</div>
-	<?php endwhile; wp_reset_postdata(); ?>
 
-	<div class='cycle-prev cycle-pagerelement'>&#8617;</div>
-	<div class='cycle-resume cycle-pagerelement' data-cycle-cmd="resume">&#187</div>
-	<div class='cycle-pause cycle-pagerelement' data-cycle-cmd="pause">&#35</div>
-	<div class='cycle-next cycle-pagerelement'>&#8618;</div>
-</div><div id="per-slide-template"></div>
+
+
+
+				// Start div.cycle-slide
+					echo '<div class="cycle-slide slide-' . get_the_ID() . '" style="background-image:url(\'' . $image_url[0] . '\');position:absolute; ">';
+
+
+						// echo '<h1>' . get_the_title() . '</h1>';
+						//the_post_thumbnail();
+						the_content();
+
+
+				// End  div.cycle-slide
+					echo '</div>';
+
+			} wp_reset_postdata(); 
+
+
+		// Cycle Pagers
+			# echo '<div class="cycle-pagerelement cycle-prev">&#8617;</div>';
+			# echo '<div class="cycle-pagerelement cycle-resume" data-cycle-cmd="resume">&#187;</div>';
+			# echo '<div class="cycle-pagerelement cycle-pause" data-cycle-cmd="pause">&#35;</div>';
+			# echo '<div class="cycle-pagerelement cycle-next">&#8618;</div>';
+
+	// End SLIDE WRAP
+		echo '</div>';
+
+		// Per Slide Template
+			# echo '<div id="per-slide-template"></div>';
+?>
