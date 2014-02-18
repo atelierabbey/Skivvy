bfe7d874ecdce5c8d5928ad69030054305b00e31
atelierabbey <info@sillycoyote.com>
2014-02-17 16:02:59 -0800

    Fixed home meta save function

--- inc/lib/mod_home_meta.php
+++ inc/lib/mod_home_meta.php
@@ -21,8 +21,10 @@
 
 
 	function __construct() {
-			add_action( 'add_meta_boxes', array( $this, 'register_home_meta' ) );
-			add_action( 'save_post', array( $this, 'save_home_meta' ) );
+			add_action('admin_enqueue_scripts',  array( $this, 'admin_scripts_home_meta' ));
+			add_action('admin_print_styles',  array( $this, 'admin_styles_home_meta' ));
+			add_action('add_meta_boxes', array( $this, 'register_home_meta' ) );
+			add_action('save_post', array( $this, 'save_home_meta' ), 10, 2 );
 
 			global $list_o_meta;
 			if (! isset($list_o_meta)) {
@@ -44,15 +46,32 @@
 			$homeid = get_option( 'page_on_front' );
 
 			if ( $homeid == $post->ID ) add_meta_box( 
-											'skivvy_home_meta',
-											'Home Meta',
-											array( $this, 'render_home_meta' ),
-											'page',
-											'normal',
-											'default'
-										);
+				'skivvy_home_meta',
+				'Home Meta',
+				array( $this, 'render_home_meta' ),
+				'page',
+				'normal',
+				'default'
+			);
 
 	}
+	public function admin_scripts_home_meta() {
+		wp_enqueue_script('media-upload');
+		wp_enqueue_script('thickbox');
+		wp_register_script('my-upload', WP_PLUGIN_URL.'/my-script.js', array('jquery','media-upload','thickbox'));
+		wp_enqueue_script('my-upload');
+	}
+	public function admin_styles_home_meta() {
+		wp_enqueue_style('thickbox');
+	}
+
+
+
+
+
+
+
+
 
 
 
@@ -62,56 +81,76 @@
 
 			global $post;
 			global $list_o_meta;
+			$home_meta_data = get_post_meta( $post->ID );
 
 			// Add an nonce field so we can check for it later.
-			wp_nonce_field( 'skivvy_home_meta_box', 'skivvy_home_meta_nonce' );
+			wp_nonce_field( basename( __FILE__ ), 'skivvy_home_meta_nonce' );
+
 			echo '<input id="HomeMetaVersion" type="hidden" value="' . skivvy_home_meta::$version . '"> ';
-			foreach ( $list_o_meta as $home_meta ) {
+
+			foreach ( $list_o_meta as $home_meta ) :
 					$slug = '_home_meta_'.sanitize_title( $home_meta );?>
 					<fieldset title="<?php echo $slug; ?>_group" class="home-meta-fieldset <?php echo $slug; ?>_group">
 						<h4><?php echo $home_meta; ?></h4>
-						<input type="text" class="home-meta-title" id="<?php echo $slug; ?>_title" name="<?php echo $slug; ?>_title" value="<?php echo esc_attr( get_post_meta( $post->ID, "{$slug}_title" , true ) ); ?>" size="25" placeholder="Title"><br>
-						<input type="text" class="home-meta-img" id="<?php echo $slug; ?>_img" name="<?php echo $slug; ?>_img" value="<?php echo esc_attr( get_post_meta( $post->ID, "{$slug}_img" , true ) ); ?>" size="25" placeholder="Image URL"><br>
-						<input type="text" class="home-meta-link" id="<?php echo $slug; ?>_link" name="<?php echo $slug; ?>_link" value="<?php echo esc_attr( get_post_meta( $post->ID, "{$slug}_link" , true ) ); ?>" size="25" placeholder="Link"><br>
-						<textarea class="home-meta-content" id="<?php echo $slug; ?>_content" name="<?php echo $slug; ?>_content" value="<?php echo esc_attr( get_post_meta( $post->ID, "{$slug}_content" , true ) ); ?>" col="63" placeholder="Content"></textarea>
+						<label for="<?php echo $slug; ?>_title">Title</label><input type="text"		class="home-meta-title"			id="<?php echo $slug; ?>_title"			name="<?php echo $slug; ?>_title"		value="<?php echo esc_attr( get_post_meta( $post->ID , "{$slug}_title" , TRUE) ); ?>"	size="25"	placeholder="Title">
+						<label for="<?php echo $slug; ?>_img">Image</label><input type="text"		class="home-meta-img"			id="<?php echo $slug; ?>_img"			name="<?php echo $slug; ?>_img"			value="<?php echo esc_attr( get_post_meta( $post->ID , "{$slug}_img" , TRUE) ); ?>"		size="25"	placeholder="Image URL">
+							<input type="button"	class="home-meta-img-button"	id="<?php echo $slug; ?>_img_button"	name="<?php echo $slug; ?>_img_button"	value="Upload" >
+						<label for="<?php echo $slug; ?>_link">Link</label><input type="text"		class="home-meta-link"			id="<?php echo $slug; ?>_link"			name="<?php echo $slug; ?>_link"		value="<?php echo esc_attr( get_post_meta( $post->ID , "{$slug}_link" , TRUE) ); ?>"	size="25"	placeholder="Link">
+						<label for="<?php echo $slug; ?>_content">Text</label><textarea				class="home-meta-content"		id="<?php echo $slug; ?>_content"		name="<?php echo $slug; ?>_content"		col="63"	placeholder="Content"><?php echo esc_attr( get_post_meta( $post->ID , "{$slug}_content" , TRUE) ); ?></textarea>
+						<script>jQuery(document).ready( function( $ ) {
+							var _custom_media = true,
+								_orig_send_attachment = wp.media.editor.send.attachment;
+
+							$('#<?php echo $slug; ?>_img_button').click(function(e) {
+								var send_attachment_bkp = wp.media.editor.send.attachment;
+								var button = $(this);
+								var id = button.attr('id').replace('_button', '');
+								_custom_media = true;
+								wp.media.editor.send.attachment = function(props, attachment){
+									if ( _custom_media ) {
+										$("#"+id).val(attachment.url);
+									} else {
+										return _orig_send_attachment.apply( this, [props, attachment] );
+									};
+							}
+
+							wp.media.editor.open(button);
+								return false;
+							});
+
+							$('.add_media').on('click', function(){
+								_custom_media = false;
+							});
+						});</script>
+
 					</fieldset>
-			<?php }
-
+			<?php
+			endforeach;
 	}
 
 
 
 
 
-	public function save_home_meta ( $post_id ) {
+	public function save_home_meta ( $post_id, $post ) {
+			global $list_o_meta;
+			$is_autosave = wp_is_post_autosave( $post_id );
+			$is_revision = wp_is_post_revision( $post_id );
+			$is_valid_nonce = ( isset( $_POST[ 'skivvy_home_meta_nonce' ] ) && wp_verify_nonce( $_POST[ 'skivvy_home_meta_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
 
-			// Check if our nonce is set.
-				if ( ! isset( $_POST['skivvy_home_meta_nonce'] ) ) return $post_id;
-
-				$nonce = $_POST['skivvy_home_meta_nonce'];
-
-			// Verify that the nonce is valid.
-				if ( ! wp_verify_nonce( $nonce, 'skivvy_home_meta_box' ) ) return $post_id;
-
-			// If this is an autosave, our form has not been submitted,
-					// so we don't want to do anything.
-				if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id;
-
-			// Check the user's permissions.
-				if ( 'page' == $_POST['post_type'] ) {
-					if ( ! current_user_can( 'edit_page', $post_id ) ) return $post_id;
-				} else {
-					if ( ! current_user_can( 'edit_post', $post_id ) ) return $post_id;
-				}
-
-			// Sanitize the user input.
-				$mydata = sanitize_text_field( $_POST['skivvy_home_meta'] );
-
-			// Update the meta field.
-				update_post_meta( $post_id, '_my_meta_value_key', $mydata );
-
+			// Verify the nonce before proceeding.
+			if ( $is_autosave || $is_revision || !$is_valid_nonce ) 
+				return;
+			
+			foreach ( $list_o_meta as $home_meta ) {
+				$slug = '_home_meta_'.sanitize_title( $home_meta );
+				//update_post_meta($post_id, $meta_key, $meta_value, $prev_value)
+				update_post_meta( $post_id, "{$slug}_title", sanitize_text_field( $_POST["{$slug}_title"] ) );
+				update_post_meta( $post_id, "{$slug}_img", sanitize_text_field( $_POST["{$slug}_img"] ) );
+				update_post_meta( $post_id, "{$slug}_link", sanitize_text_field( $_POST["{$slug}_link"] ) );
+				update_post_meta( $post_id, "{$slug}_content", sanitize_text_field( $_POST["{$slug}_content"] ) );
+			}
 	}
-
 
 
 } endif;?>
