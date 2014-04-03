3408d22cad3df264703489abbd3d98d6b4e026de
atelierabbey <info@sillycoyote.com>
2014-04-03 10:54:48 -0700

    Added gallery html5 support

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php #18Feb14
 add_action( 'after_setup_theme', 'skivvy_setup' ); function skivvy_setup() {
 		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
-	#	include 'inc/skivvy_autooptions.php';
+		add_theme_support( 'html5', array('comment-list','search-form','comment-form','gallery') );
 	#	add_theme_support( 'post-formats', array('aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video') );
-		add_filter('widget_text', 'do_shortcode'); // Widget Support - Shortcodes 
+	#	include 'inc/skivvy_autooptions.php';
+	#	add_filter('widget_text', 'do_shortcode'); // Widget Support - Shortcodes 
 	#	add_filter('widget_text', array( $wp_embed, 'run_shortcode' ), 8 );
 	#	add_filter('widget_text', array( $wp_embed, 'autoembed'), 8 ); // Widget Support - oEmbed & oEmbed 2
 	#	$lang_location = get_template_directory_uri() . '/inc'; load_theme_textdomain( 'skivvy', $lang_location ); $locale_file = $lang_location . '/' . get_locale() . '.php'; if ( is_readable( $locale_file )){ require_once( $locale_file );}
@@ -20,11 +20,11 @@ add_action( 'after_setup_theme', 'skivvy_setup' ); function skivvy_setup() {
 		include 'inc/skivvy_branding.php';
 
 // Library - It's not nice to write in things from the library.
-		include 'inc/lib/skivvy_websiteoptions.php'; new skivvy_websiteoptions;
+	#	include 'inc/lib/skivvy_websiteoptions.php'; new skivvy_websiteoptions;
 	#	include 'inc/lib/skivvy_home_meta.php'; $list_o_meta = array('Bucket 1','Bucket 2','Bucket 3','Bucket 4'); new skivvy_home_meta ;
 	#	include 'inc/lib/skivvy_toolbox.php';
 	#	include 'inc/lib/skivvy_toolbox-styles.php'; 
 	#	include 'inc/lib/skivvy_toolbox-posttypes.php';
 	#	include 'inc/lib/mod_buckets.php';
-		include 'inc/lib/mod_browserdetect.php';
+	#	include 'inc/lib/mod_browserdetect.php';
 ?>
