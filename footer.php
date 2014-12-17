85662089ed9a796e6675b19b505e2a511565a8fd
atelierabbey <info@sillycoyote.com>
2014-12-17 08:56:42 -0800

    Added nofollow to credentials

--- footer.php
+++ footer.php
@@ -10,8 +10,8 @@
 			<div class="clear"></div>
 		</div>
 		<div class="footer-copyright"><?php
-	
-			// Auto generate year. if 
+
+			// Auto generate year. if
 			$start_year = "2014";
 			$date = date( 'Y' );
 			if ( $date > $start_year ) {
@@ -20,12 +20,12 @@
 
 			_e( sprintf( 'Copyright &copy; %1$s <a href="%2$s" title="%3$s" rel="home">%4$s</a>. All Rights Reserved. ' , $date, home_url('/'), esc_attr( get_bloginfo( 'name', 'display' ) ), get_bloginfo( 'name' )), 'skivvy' );
 
-			_e( '<a href="http://www.d4webdesign.com/" title="Reno Web Design" target="_blank">Web Design</a> by D4', 'skivvy' ) ;
+			_e( '<a href="http://www.d4webdesign.com/" title="Reno Web Design" rel="nofollow" target="_blank">Web Design</a> by D4', 'skivvy' ) ;
 
 		?></div>
 	</div>
 </footer>
-<?php 
+<?php
 
 	// includes Analytics.php code
 		require ( 'js/analytics.php' );
