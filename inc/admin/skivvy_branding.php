7fc80d2196b00acca279d1506863582a19e4194c
atelierabbey <info@sillycoyote.com>
2014-12-16 16:46:48 -0800

    moved HTML classes, hide rss feed widget

--- inc/admin/skivvy_branding.php
+++ inc/admin/skivvy_branding.php
@@ -1,4 +1,4 @@
-<?php #12Jun14
+<?php #16Dec14
 
 /*
  *
@@ -7,57 +7,21 @@
  *
  */
 function skinfo($what='Version') {
-	$ct = wp_get_theme(); 
+	$ct = wp_get_theme();
 	switch ($what){
 		case 'Name': return $ct->Name; break; //Theme name as given in theme's style.css
-		case 'ThemeURI': return $ct->ThemeURI; break; //The path to the theme's directory 
-		case 'Description': return $ct->Description; break; // The description of the theme 
+		case 'ThemeURI': return $ct->ThemeURI; break; //The path to the theme's directory
+		case 'Description': return $ct->Description; break; // The description of the theme
 		case 'Author': return $ct->display( 'Author', FALSE ); break; //The theme's author
 		case 'AuthorURI': return $ct->get( 'AuthorURI' ); break; // The website of the theme author
 		case 'Version': return $ct->Version; break; //The version of the theme
 		case 'Template': return $ct->Template; break; //The folder name of the current theme
-		case 'Status': return $ct->Status; break; //If the theme is published 
-		case 'Tags': return $ct->Tags; break; //Tags used to describe the theme 
-		case 'TextDomain': return $ct->get( 'TextDomain' ); break; //The text domain used in the theme for translation purposes 
-		case 'DomainPath': return $ct->get( 'DomainPath'); break; //Path to the theme translation files 
+		case 'Status': return $ct->Status; break; //If the theme is published
+		case 'Tags': return $ct->Tags; break; //Tags used to describe the theme
+		case 'TextDomain': return $ct->get( 'TextDomain' ); break; //The text domain used in the theme for translation purposes
+		case 'DomainPath': return $ct->get( 'DomainPath'); break; //Path to the theme translation files
 	}
 }
-
-
-
-
-/*
-	// Add specific CSS class by filter
-	add_filter('html_classes','my_class_names');
-	function my_class_names($classes) {
-		// add 'class-name' to the $classes array
-		$classes[] = 'class-name';
-		// return the $classes array
-		return $classes;
-	}
-*/
-function html_classes( $class = array() ) {
-
-	$classes = array();
-
-	// Adds Skivvy version
-	$classes[] = skinfo('Version');
-
-
-	if ( ! empty( $class ) ) {
-			if ( !is_array( $class ) )
-					$class = preg_split( '#\s+#', $class );
-			$classes = array_merge( $classes, $class );
-	}
-
-	$classes = array_map( 'esc_attr', $classes );
-	$all_classes = apply_filters( 'html_classes', $classes, $class );
-
-	// Separates classes with a single space, collates classes
-	echo join( ' ', $all_classes );
-}
-
-
 
 
 
@@ -109,7 +73,7 @@ function html_classes( $class = array() ) {
 
 
 	// Adds a warning box to the settings page Uses the same style box as the WordPress Update "update-nag"
-	function skivvy_admin_notice(){ 
+	function skivvy_admin_notice(){
 		global $current_screen;
 		if ( $current_screen->parent_base == 'options-general' ){
 			echo '<div id="admin-settings-warning-box"><strong>Warning</strong> - changing settings on these pages may cause problems with your website&rsquo;s design!</div>';
@@ -122,7 +86,7 @@ function html_classes( $class = array() ) {
 	if (!current_user_can('edit_users')) {
 		add_action('init', create_function('$a', "remove_action('init', 'wp_version_check');"), 2);
 		add_filter('pre_option_update_core', create_function('$a', "return null;"));
-	} 
+	}
 	***/
 
 
@@ -150,8 +114,8 @@ function html_classes( $class = array() ) {
 
 		if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
-			// Figure out how many total items there are, but limit it to 5. 
-			$maxitems = $rss->get_item_quantity( 5 ); 
+			// Figure out how many total items there are, but limit it to 5.
+			$maxitems = $rss->get_item_quantity( 5 );
 
 			// Build an array of all the items, starting with element 0 (first element).
 			$rss_items = $rss->get_items( 0, $maxitems );
@@ -178,7 +142,7 @@ function html_classes( $class = array() ) {
 
 	// Registers those widgets
 	function skivvy_dashboard_widgets() {
-		wp_add_dashboard_widget('dashboard_custom_feed', skinfo('Author').' News Feed', 'skivvy_dashboard_feed');
+		# wp_add_dashboard_widget('dashboard_custom_feed', skinfo('Author').' News Feed', 'skivvy_dashboard_feed');
 		wp_add_dashboard_widget('dashboard_custom_help', 'Website Technical Support', 'skivvy_dashboard_help');
 	} add_action('wp_dashboard_setup', 'skivvy_dashboard_widgets');
 
--- inc/lib/skivvy_func_user.php
+++ inc/lib/skivvy_func_user.php
@@ -1,69 +1,332 @@
-<?php #8Sep14
+<?php #16Dec14
 /*
  *		-------------------------------------------------------
- *		Usable functions
+ *		Auto Functions
  *		-------------------------------------------------------
  */
 
-//// ---- the_snippet() ---- ////  function to replace the_excerpt(), ex. the_snippet(72,'Read More');
-function the_snippet($length=55,$readmore='Read More') {
-	global $post;
-	$text = $post->post_content;
-	$text = strip_shortcodes( $text );
-	$text = apply_filters('the_content', $text);
-	$text = str_replace(']]>', ']]&gt;', $text);
-	$more_link = '... <a href="'.get_permalink($post->ID).'" class="readmorebtn">'.$readmore.'</a>';
-	echo wp_trim_words($text,$length,$more_link); 
-} 
+// Skivvy Body Classes
+	function skivvy_body_classes($classes) {
+		global $wpdb, $post;
 
-// ---- get_the_thumbnail_caption() ---- //// Returns the caption for attached featured image featured image
-function get_the_thumbnail_caption() {
-	global $post;
+		if (is_page()) {
 
-	$thumbnail_id    = get_post_thumbnail_id($post->ID);
-	$thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
+			// .section-{$parentpage} - Parent Page Post class -- Add the top level parent page to the body class
+				if ($post->post_parent) {
+					$parent  = end(get_post_ancestors($current_page_id));
+				} else {
+					$parent = $post->ID;
+				}
+				$post_data = get_post($parent, ARRAY_A);
+				$classes[] = 'section-' . $post_data['post_name'];
 
-	if ($thumbnail_image && isset($thumbnail_image[0])) {
-		$caption = $thumbnail_image[0]->post_excerpt;
+			// .subpage page class
+				if ( ! is_front_page() ) {
+					$classes[] = 'subpage';
+				}
+
+		}
+
+		return $classes;
+
 	}
-	return $caption;
+	add_filter('body_class','skivvy_body_classes');
+
+
+/*
+	// Add specific CSS class by filter
+	add_filter('html_classes','my_class_names');
+	function my_class_names($classes) {
+		// add 'class-name' to the $classes array
+		$classes[] = 'class-name';
+		// return the $classes array
+		return $classes;
+	}
+*/
+function html_classes( $class = array() ) {
+
+	$classes = array();
+
+	// Adds Skivvy version
+	if (function_exists('skinfo')){
+		$classes[] = skinfo('Version');
+	}
+
+
+	if ( ! empty( $class ) ) {
+			if ( !is_array( $class ) )
+					$class = preg_split( '#\s+#', $class );
+			$classes = array_merge( $classes, $class );
+	}
+
+	$classes = array_map( 'esc_attr', $classes );
+	$all_classes = apply_filters( 'html_classes', $classes, $class );
+
+	// Separates classes with a single space, collates classes
+	echo join( ' ', $all_classes );
 }
 
 
+
+
+//// ---- Add featured images to RSS feed ---- ////
+	function rss_post_thumbnail($content) {
+		global $post;
+		if(has_post_thumbnail($post->ID)) {
+			$content = '<p>' . get_the_post_thumbnail($post->ID) .'</p>' . get_the_content();
+		}
+		return $content;
+	}
+	add_filter('the_excerpt_rss', 'rss_post_thumbnail');
+	add_filter('the_content_feed', 'rss_post_thumbnail');
+
+
+
+
+
+//// ---- Advanced Menu Styling Classes ---- ////
+	if( class_exists( 'SimpleXMLElement' ) ) {
+		// Cleans up and counts number of menu items
+		function skivvy_advmenus( $output ) {
+				libxml_use_internal_errors( false );
+				try {
+					$xml = new SimpleXMLElement( preg_replace( '#>([^<]+)<#i', '><![CDATA[\\1]]><', $output ), LIBXML_NOWARNING );
+				}
+				catch( Exception $e ) {
+					return $output;
+				}
+				$container = array();
+				if( ! $xml->li ) {
+					list( , $item ) = each( $xml->xpath( 'ul' ) );
+					if( ! $item ) list( , $item ) = each( $xml->xpath( 'menu' ) );
+					if( $item ) {
+						$container = array( '<' . $xml->getName(), '</' . $xml->getName() . '>' );
+						foreach( $xml->attributes() as $key => $value ) $container[0] .= ' ' . $key . '="' . $value . '"';
+						$container[0] .= '>';
+				}
+				}
+				else $item = $xml;
+				if( count( $item ) ) return $container[0]. preg_replace( '#<\?[^>]*\?>#', '', preg_replace( '#<!\[CDATA\[([^<]+)\]\]>#', '\\1', skivvy_advmenus_level( $item )->asXML() ) ) . $container[1];
+				else return $container[0] . $output . $container[1];
+			} add_filter( 'wp_nav_menu', 'skivvy_advmenus' );
+
+		// Adds .odd-menu-item , .even-menu-item , .last-menu-item , .first-menu-item  to the Nav menu
+			function skivvy_advmenus_level( $xml ) {
+
+				if( 0 < $count = count( $xml->li ) ) {
+					$i = 1;
+					foreach( $xml->li as $item ) {
+						$attributes = $item->attributes();
+						// Adds Even Odd classes
+							if( $i % 2 ) $attributes['class'] = 'odd-menu-item ' . $attributes['class'];
+							else $attributes['class'] = 'even-menu-item ' . $attributes['class'];
+
+						// Adds parent class if has submenu
+							if( $item->ul ) {
+								$attributes['class'] = 'parent-menu-item ' . $attributes['class'];
+								skivvy_advmenus_level( $item->ul );
+							}
+							elseif( $item->menu ) {
+								$attributes['class'] = 'parent-menu-item ' . $attributes['class'];
+								skivvy_advmenus_level( $item->menu );
+							}
+
+						// Adds First and Last class
+							if( $i == $count ) $attributes['class'] = 'last-menu-item ' . $attributes['class'];
+							if( $i == 1 ) $attributes['class'] = 'first-menu-item ' . $attributes['class'];
+							$i ++;
+					}
+				}
+				return $xml;
+
+			}
+
+	} // Ends if class_exists('SimpleXMLElement') */
+
+
+
+
 /*
- *		-------------------------------------------------------
- *		WordPress Functional Extensions
- *		-------------------------------------------------------
- */
+**
+**		14Apr14
+**
+*/
+# - http://www.useragentstring.com/pages/useragentstring.php
+	function css_browser_selector( $classes ) {
 
-//// ---- Mobile Menu Walker Menu ---- ///
-class Walker_Nav_Mobile extends Walker_Nav_Menu {
-	function start_lvl(&$output, $depth){
-		$indent = str_repeat("\t", $depth); // don't output children opening tag (`<ul>`)
-	}
- 
-	function end_lvl(&$output, $depth){
-		$indent = str_repeat("\t", $depth); // don't output children closing tag
-	}
- 
-	/**
-	* Start the element output.
-	*
-	* @param  string $output Passed by reference. Used to append additional content.
-	* @param  object $item   Menu item data object.
-	* @param  int $depth     Depth of menu item. May be used for padding.
-	* @param  array $args    Additional strings.
-	* @return void
-	*/
-	function start_el(&$output, $item, $depth, $args) {
- 		$url = '#' !== $item->url ? $item->url : '';
- 		$output .= '<option value="' . $url . '">' . $item->title;
-	}	
- 
-	function end_el(&$output, $item, $depth){
-		$output .= "</option>\n"; // replace closing </li> with the option tag
-	}
-} 
+			// $ua = null
+			$ua = ($ua) ? strtolower($ua) : strtolower($_SERVER['HTTP_USER_AGENT']);
 
+			$g = 'gecko';
+			$w = 'webkit';
+			$s = 'safari';
+			if (is_array($classes)) {
+				$b = $classes;
+			} else {
+				$b = array();
+			}
+				//$b[] = $ua;
+			// browser
+
+			// ie11 - 26Feb14 mozilla/5.0 (windows nt 6.3; wow64; trident/7.0; matbjs; rv:11.0) like gecko gecko win
+			//ie10 - 26Feb14 mozilla/5.0 (compatible; msie 10.0; windows nt 6.2; trident/6.0) ie ie1 win
+			//ie9  - 26Feb14 mozilla/5.0 (compatible; msie 9.0; windows nt 6.1; trident/5.0) ie ie9 win
+			//ie8  - 26Feb14 mozilla/4.0 (compatible; msie 8.0; windows nt 6.1; trident/4.0) ie ie8 win
+			if(!preg_match('/opera|webtv/i', $ua) && preg_match('/msie\s(\d)/', $ua, $array)) {
+					$b[] = 'ie';
+					$b[] = 'ie'.$array[1];
+			}	else if(strstr($ua, 'firefox/27')) {
+					$b[] = 'ff27';
+			}	else if(strstr($ua, 'firefox/2 ')) {
+					$b[] = $g . 'ff2';
+			}	else if(strstr($ua, 'firefox/3.5')) {
+					$b[] = $g . 'ff3 ff3_5';
+			}	else if(strstr($ua, 'firefox/3 ')) {
+					$b[] = $g . 'ff3';
+			} else if(strstr($ua, 'gecko/')) {
+					$b[] = $g;
+			} else if(preg_match('/opera(\s|\/)(\d+)/', $ua, $array)) {
+					$b[] = 'opera opera' . $array[2];
+			} else if(strstr($ua, 'konqueror')) {
+					$b[] = 'konqueror';
+			} else if(strstr($ua, 'chrome')) {
+					$b[] = $w . ' ' . $s . ' chrome';
+			} else if(strstr($ua, 'iron')) {
+					$b[] = $w . ' ' . $s . ' iron';
+			} else if(strstr($ua, 'applewebkit/')) {
+					$b[] = (preg_match('/version\/(\d+)/i', $ua, $array)) ? $w . ' ' . $s . ' ' . $s . $array[1] : $w . ' ' . $s;
+			} else if(strstr($ua, 'mozilla/')) {
+					$b[] = $g;
+			}
+
+			// platform
+			if(strstr($ua, 'j2me')) {
+					$b[] = 'mobile';
+			} else if(strstr($ua, 'iphone')) {
+					$b[] = 'iphone';
+			} else if(strstr($ua, 'ipod')) {
+					$b[] = 'ipod';
+			} else if(strstr($ua, 'mac')) {
+					$b[] = 'mac';
+			} else if(strstr($ua, 'darwin')) {
+					$b[] = 'mac';
+			} else if(strstr($ua, 'webtv')) {
+					$b[] = 'webtv';
+			} else if(strstr($ua, 'win')) {
+					$b[] = 'win';
+			} else if(strstr($ua, 'freebsd')) {
+					$b[] = 'freebsd';
+			} else if(strstr($ua, 'x11') || strstr($ua, 'linux')) {
+					$b[] = 'linux';
+			}
+
+			return $b;
+
+
+
+			/*
+			function get_browser($returnValue = 'class')
+		{
+			if(empty($_SERVER['HTTP_USER_AGENT'])) return false;
+
+			$u_agent = $_SERVER['HTTP_USER_AGENT'];
+			$bname = 'Unknown';
+			$platform = 'Unknown';
+			$version= "";
+
+			//First get the platform?
+			if (preg_match('!linux!i', $u_agent)) {
+				$platform = 'linux';
+			}
+			elseif (preg_match('!macintosh|mac os x!i', $u_agent)) {
+				$platform = 'mac';
+			}
+			elseif (preg_match('!windows|win32!i', $u_agent)) {
+				$platform = 'windows';
+			}
+
+			// Next get the name of the useragent yes seperately and for good reason
+			if(preg_match('!MSIE!i',$u_agent) && !preg_match('!Opera!i',$u_agent))
+			{
+				$bname = 'Internet Explorer';
+				$ub = "MSIE";
+			}
+			elseif(preg_match('!Firefox!i',$u_agent))
+			{
+				$bname = 'Mozilla Firefox';
+				$ub = "Firefox";
+			}
+			elseif(preg_match('!Chrome!i',$u_agent))
+			{
+				$bname = 'Google Chrome';
+				$ub = "Chrome";
+			}
+			elseif(preg_match('!Safari!i',$u_agent))
+			{
+				$bname = 'Apple Safari';
+				$ub = "Safari";
+			}
+			elseif(preg_match('!Opera!i',$u_agent))
+			{
+				$bname = 'Opera';
+				$ub = "Opera";
+			}
+			elseif(preg_match('!Netscape!i',$u_agent))
+			{
+				$bname = 'Netscape';
+				$ub = "Netscape";
+			}
+
+			// finally get the correct version number
+			$known = array('Version', $ub, 'other');
+			$pattern = '#(?<browser>' . join('|', $known) .
+			')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+			if (!@preg_match_all($pattern, $u_agent, $matches)) {
+				// we have no matching number just continue
+			}
+
+			// see how many we have
+			$i = count($matches['browser']);
+			if ($i != 1) {
+				//we will have two since we are not using 'other' argument yet
+				//see if version is before or after the name
+				if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+					$version= $matches['version'][0];
+				}
+				else {
+					$version= $matches['version'][1];
+				}
+			}
+			else {
+				$version= $matches['version'][0];
+			}
+
+			// check if we have a number
+			if ($version==null || $version=="") {$version="?";}
+
+			$mainVersion = $version;
+			if (strpos($version, '.') !== false)
+			{
+				$mainVersion = explode('.',$version);
+				$mainVersion = $mainVersion[0];
+			}
+			if($returnValue == 'class')
+			{
+				return $ub." ".$ub.$mainVersion;
+			}
+			else
+			{
+			return array(
+				'userAgent' => $u_agent,
+				'name'	  => $bname,
+				'version'   => $version,
+				'platform'  => $platform,
+				'pattern'   => $pattern
+			);
+		}
+	}
+		*/
+}
+add_filter('html_classes','css_browser_selector', 10 );
 
 ?>
