b2cc65ea5205980fa395a54a5abedaf25b73a156
SillyCoyote <sungsungs3+github@gmail.com>
2015-05-13 14:06:05 -0700

    cleaned up function text

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php #13May15
 	include 'lib/skivvy_func_auto.php';		// Auto Functions
 	include 'lib/skivvy_func_user.php';		// Usable Functions
-	include 'lib/admin/index.php';			// Admin functions & styling
+	include 'lib/admin/index.php';			// Admin Functions & styling
 	include 'lib/skivvy_shortcodes.php';	// Shortcodes
-	include 'inc/skivvy_simple.php';		// Various areas of clean up code, admin area, and various other functions of unneeded defaults
-	include 'inc/skivvy_register.php';		// Registry for theme setup, posttypes, taxonomy, menus, widget areas, scripts, styles, etc.
-
+	include 'inc/skivvy_simple.php';		// Cleanup
+	include 'inc/skivvy_register.php';		// Registry
 ?>
