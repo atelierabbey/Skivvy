9af492e04ef41454cb37049c91f8da5b026682ee
SillyCoyote <SillyCoyote@users.noreply.github.com>
2015-05-24 22:52:16 -0700

    Added Query string to html_classes

--- inc/skivvy_register.php
+++ inc/skivvy_register.php
@@ -56,6 +56,12 @@
 //  REGISTER - HTML Classes 
 	function skivvy_html_classes( $classes ) {
 
+		$query = $_SERVER["QUERY_STRING"];
+		
+		if ( $query != '' ) {
+			$classes = array_merge( $classes, explode( '&', str_replace( '=' , '_' , $query ) ) );
+		}
+
 		return $classes;
 	} add_filter('html_classes','skivvy_html_classes');
 
