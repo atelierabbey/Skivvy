fc84563d249b9a92ae63940357e47ea519b11c50
Grayson A.C. Laramore <info@sillycoyote.com>
2015-11-10 21:15:19 -0800

    Moved Register and Simple

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 	include 'lib/skivvy_func_auto.php';		// Auto Functions
 	include 'lib/skivvy_func_user.php';		// Usable Functions
 	include 'lib/admin/index.php';			// Admin Functions & Styling
-	include 'inc/skivvy_simple.php';		// Cleanup
-	include 'inc/skivvy_register.php';		// Registry
+	include 'lib/skivvy_simple.php';		// Cleanup
+	include 'lib/skivvy_register.php';		// Registry
 ?>
