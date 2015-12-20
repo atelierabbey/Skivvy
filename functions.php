52fd57c91904e69374fd5e9ab1cf527236969184
Coyoli <info@sillycoyote.com>
2015-12-19 16:48:16 -0800

    Nav Menu Walker initial setup

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 	include 'lib/skivvy_func_auto.php';		// Auto Functions
 	include 'lib/skivvy_func_user.php';		// Usable Functions
+	include 'lib/skivvy_menu_render.php';	// Nav Menu functions
 	include 'lib/admin/index.php';			// Admin Functions & Styling
 	include 'lib/skivvy_simple.php';		// Cleanup
 	include 'lib/skivvy_register.php';		// Registry
