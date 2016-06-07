c88852edcfc84e4cfc90098c3c1677fc7de87b5e
Grayson A.C. Laramore <info@sillycoyote.com>
2016-06-06 22:24:52 -0700

    converted header to use $output

--- header.php
+++ header.php
@@ -15,21 +15,21 @@
 	wp_head();
 ?></head>
 <body id="page-<?php the_ID(); ?>" <?php body_class(); ?>>
-<header role="banner" id="header">
-	<div class="page-wrapper"><?php
+<?php
+$output .= '<header role="banner" id="header">';
+	$output .= '<div class="page-wrapper">'
 
-		echo (
 			// Logo
-				'<a id="logo" class="alignleft" href="' . home_url( '/' ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" rel="home">' .
-					'<img src="' . get_stylesheet_directory_uri() . '/img/logo.png" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '">' .
-				'</a>'.
+				$output .= '<a id="logo" class="alignleft" href="' . home_url( '/' ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" rel="home">';
+					$output .= '<img src="' . get_stylesheet_directory_uri() . '/img/logo.png" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '">';
+				$output .= '</a>';
 
 			// Mobile Toggle
-				'<button id="mobile-toggle" class="alignright dont-print"></button>'.
+				$output .= '<button id="mobile-toggle" class="alignright dont-print"></button>';
 
 			// Main Menu
-				'<nav role="navigation" class="dont-print">'.
-						wp_nav_menu( array(
+				$output .= '<nav role="navigation" class="dont-print">';
+					$output .= wp_nav_menu( array(
 							'menu_class'      => 'alignright nobull dropdown animated flyoutleft', // Menu functionality classes
 							'menu_id'         => 'main-menu',
 							'theme_location'  => 'main',
@@ -39,12 +39,12 @@
 							'depth'           => 3, // 0 = all. Default, -1 = displays links at any depth and arranges them in a single, flat list.
 							'walker'          => new skivvy_walker_main
 
-						)).
+					));
 
-					'<div class="clear"></div>'.
+					$output .= '<div class="clear"></div>';
 
 					// Mobile Menu
-						wp_nav_menu( array(
+					$output .= wp_nav_menu( array(
 							'container'       => false,
 							'menu_class'      => 'nobull textcenter clearfix',
 							'menu_id'         => 'mobile-nav',
@@ -52,9 +52,11 @@
 							'theme_location'  => 'mobile',
 							'depth'           => -1,
 							'echo'            => false
-						)).
-				'</nav>'
-		);
+					));
+				$output .= '</nav>';
 
-	?></div>
-</header>
+
+	$output .= '</div>';
+$output .= '</header>';
+echo $output;
+?>
