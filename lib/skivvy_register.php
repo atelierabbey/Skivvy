37b0a939807f9df5c9385ce71d8e5031cd537122
Grayson A.C. Laramore <info@sillycoyote.com>
2016-06-13 22:17:39 -0700

    rearranged registry

--- lib/skivvy_register.php
+++ lib/skivvy_register.php
@@ -1,16 +1,18 @@
 <?php # 10Nov15
-// http://generatewp.com
 //		Menu
+//		WP_HEAD()
 //		Javascripts & Styles
+//		HTML Classes
+//		Body Classes
+//		WP_FOOT()
 //		Widgets Area
 //		MIME Types
-//		Post Types
 //		Theme Setup
 
 
 
 
-//	REGISTER - Menus
+// Menus
 	register_nav_menus( array(
 		'main' => __( 'Main Menu' ),
 		'mobile' =>  __( 'Mobile Menu' ),
@@ -19,8 +21,57 @@
 
 
 
+// WP_HEAD()
+	function skivvy_head() {
 
-//	REGISTER - Javascripts & Styles
+		$head_meta = array(
+			// Standard Meta
+				'<meta charset="'. get_bloginfo( 'charset' ). '">', // HTML character set encoding. NOTE - Needs to be close to the opening HEAD tag
+				'<meta name="description" content="' . get_bloginfo( 'description', 'display' ) . '">'. // Meta description, important for SEO. Defaults to blog's description.
+
+			// Mobile Meta
+				'<meta content="width=device-width, initial-scale=1.0" name="viewport">', // Sets default width and scale to be dependent on the device.
+			#	'<meta name="format-detection" content="telephone=no"><meta http-equiv="x-rim-auto-match" content="none">', // Don't autodetect phonenumbers and create links in iphone safari & blackberry
+
+			// IE specific
+				'<!--[if IE]><meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"><![endif]-->', // Force IE to render most recent engine for installed browser. And enable Chrome Frame
+				'<!--[if lt IE 9]><script src="' . get_template_directory_uri() . '/js/html5.js"></script><![endif]-->', // HTML5 Shiv for < IE9
+
+			// WordPress items
+			#	'<link rel="profile" href="http://gmpg.org/xfn/11">',
+
+			// Google fonts
+				"<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>",
+
+
+			// BROWSER COLORS - https://developers.google.com/web/fundamentals/design-and-ui/browser-customization/theme-color?hl=en
+				'<meta name="theme-color" content="#4285f4">', // Chrome, Firefox OS and Opera
+				'<meta name="msapplication-navbutton-color" content="#4285f4">',  // Windows Phone
+			#	'<meta name="apple-mobile-web-app-capable" content="yes">', // iOS Safari
+			#	'<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">', // iOS Safari
+
+		);
+
+		// Prevent Duplicate Content - Source: http://perishablepress.com/seo-experiment-let-google-sort-it-out/
+			if ((is_home() && ($paged < 2 )) || is_single() || is_page() || is_category()) {
+				$head_meta[] = '<meta name="robots" content="index,archive,follow" />';
+			} else {
+				$head_meta[] = '<meta name="robots" content="noindex,noarchive,follow" />';
+			}
+
+		// Echo each with a linebreak every line
+		foreach ($head_meta as $meta) {
+			echo $meta. "\r\n";
+		}
+
+
+
+	} add_action( 'wp_head', 'skivvy_head', 0 );
+	  add_action( 'wp_head', 'wp_site_icon');
+
+
+
+// Javascripts & Styles
 	function skivvy_scriptnstyle_enqueuer() {
 
 		// SCRIPTS - wp_register_script( $handle, $src, $deps, $ver, $in_footer );
@@ -41,73 +92,30 @@
 
 
 
-//  REGISTER - HTML Classes 
+// HTML Classes 
 	function skivvy_html_classes( $classes ) {
 
+		// Adds .autohide-adminbar when logged in
+			if ( is_user_logged_in() ) {
+				#	$classes[] = 'autohide-adminbar';
+			}
+
+		// break query string into a classes
 		$query = $_SERVER["QUERY_STRING"];
-		
 		if ( $query != '' ) {
 			$classes = array_merge( $classes, explode( '&', str_replace( '=' , '_' , $query ) ) );
 		}
 
-		return $classes;
+
+		// Adds Skivvy version
+			if (function_exists('skinfo')){
+				$classes[] = skinfo('Version');
+			}
+
+			return $classes;
+
 	} add_filter('html_classes','skivvy_html_classes');
 
-
-
-
-//  WP_HEAD()
-
-	function skivvy_head() {
-		echo (
-			'<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">' // Force IE to render most recent engine for installed browser. And enable Chrome Frame
-			.'<meta content="width=device-width, initial-scale=1.0" name="viewport">' // Sets default width and scale to be dependent on the device.
-		#	.'<meta name="format-detection" content="telephone=no"><meta http-equiv="x-rim-auto-match" content="none">' // Don't autodetect phonenumbers and create links in iphone safari & blackberry
-		#	.'<link rel="profile" href="http://gmpg.org/xfn/11">'
-
-			.'<!--[if lt IE 9]><script src="' . get_template_directory_uri() . '/js/html5.js"></script><![endif]-->' // HTML5 Shiv for < IE9
-
-		);
-
-		// Prevent Duplicate Content - Source: http://perishablepress.com/seo-experiment-let-google-sort-it-out/
-		if ((is_home() && ($paged < 2 )) || is_single() || is_page() || is_category()) {
-			echo '<meta name="robots" content="index,archive,follow" />';
-		} else {
-			echo '<meta name="robots" content="noindex,noarchive,follow" />';
-		}
-
-
-	} add_action( 'wp_head', 'skivvy_head' );
-
-
-// Skivvy HTML Classes
-	function html_classes( $class = array() ) {
-
-	$classes = array();
-
-	// Adds Skivvy version
-	if (function_exists('skinfo')){
-		$classes[] = skinfo('Version');
-	}
-
-	// Adds .autohide-adminbar when logged in
-	/*
-	if ( is_user_logged_in() ) {
-		$classes[] = 'autohide-adminbar';
-	} //*/
-
-	if ( ! empty( $class ) ) {
-			if ( !is_array( $class ) )
-				$class = preg_split( '#\s+#', $class );
-			$classes = array_merge( $classes, $class );
-	}
-
-	$classes = array_map( 'esc_attr', $classes );
-	$all_classes = apply_filters( 'html_classes', $classes, $class );
-
-	// Separates classes with a single space, collates classes
-	echo join( ' ', $all_classes );
-}
 
 
 // Body Classes
@@ -135,21 +143,19 @@
 
 		return $classes;
 
-	}
-	add_filter('body_class','skivvy_body_classes');
+	} add_filter('body_class','skivvy_body_classes');
 
 
 
-//  WP_FOOT()
+// WP_FOOT()
 	function skivvy_foot() {
 
 	} add_action( 'wp_footer', 'skivvy_foot' );
 
 
 
-
-// REGISTER - Widget Areas
-/*
+// Widget Areas
+	/*
 	function skivvy_register_sidebars() {
 		register_sidebar( array(
 			'name' => __( 'Sidebar name', 'skivvy' ),
@@ -164,17 +170,52 @@
 
 
 
+// MIME Types
+	function skivvy_add_mime_types($mimes){
+		// Added wp-upload MIME types - http://skivvy.atelierabbey.org/filters/mime-types/
+		return array_merge($mimes,array (
+			'swf' => 'application/x-shockwave-flash',
+			'eps' => 'application/postscript',
+			'svg' => 'image/svg+xml',
+		));
+	} add_filter('upload_mimes','skivvy_add_mime_types');
 
-// Basic Theme Setup Setup
+
+
+// Theme Setup
 	function skivvy_setup() {
 
 		// WP-Theme Support - http://codex.wordpress.org/Function_Reference/add_theme_support
+				add_theme_support( 'custom-logo', array(
+				#	'height'      => 175,
+				#	'width'       => 400,
+				#	'flex-height' => true,
+				#	'flex-width'  => true,
+				#	'header-text' => array( 'site-title', 'site-description' ),
+				) ); //*/
+
+				/*add_theme_support( 'custom-header', array(
+				#	'default-image'          => '',
+				#	'width'                  => 0,
+				#	'height'                 => 0,
+				#	'flex-height'            => false,
+				#	'flex-width'             => false,
+				#	'uploads'                => true,
+				#	'random-default'         => false,
+				#	'header-text'            => true,
+				#	'default-text-color'     => '',
+				#	'wp-head-callback'       => '',
+				#	'admin-head-callback'    => '',
+				#	'admin-preview-callback' => '',
+				) ); //*/
 				add_theme_support( 'title-tag' );
 				add_theme_support( 'post-thumbnails' );
 				add_theme_support( 'html5', array('search-form','comment-form','comment-list','gallery','caption','widgets') );
 				add_theme_support( 'automatic-feed-links' );
 			#	add_theme_support( 'post-formats', array('aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video') );
 			#	add_theme_support( 'woocommerce' );
+
+
 
 		// Add Widget/sidebar functionality
 			#	add_filter('widget_text', 'do_shortcode'); // Widget Support - Shortcodes
@@ -183,5 +224,3 @@
 
 	} add_action( 'after_setup_theme', 'skivvy_setup' );
 
-
-?>
