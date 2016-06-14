68f2924d1fc373bfd81249add23ef2b88b76aec6
Grayson A.C. Laramore <info@sillycoyote.com>
2016-06-13 20:41:00 -0700

    separated lines into rearrangeable lines

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 
 get_header();
 
-	echo ('<section id="content">'.
-			'<div class="page-wrapper">'.
-				'<main role="main" id="main-content" class="clearfix">');
+	echo '<section id="content">';
+		echo '<div class="page-wrapper">';
+				echo '<main role="main" id="main-content" class="clearfix">';
 
-					if ( ! is_front_page() ) {
-						get_template_part( 'inc/chunk' , 'title' );
-					} 
+						if ( ! is_front_page() ) {
+							get_template_part( 'inc/chunk' , 'title' );
+						} 
 
-					get_template_part( 'inc/chunk' , 'content' );
+						get_template_part( 'inc/chunk' , 'content' );
 
-					if ( ! is_page() ) get_template_part( 'inc/chunk' , 'pagination' );
+						if ( ! is_page() ) get_template_part( 'inc/chunk' , 'pagination' );
 
-			echo '</main>';
-			# get_sidebar();
-	echo ('</div>'.
-	'</section>');
+				echo '</main>';
+				# get_sidebar();
+		echo '</div>';
+	echo '</section>';
 
 get_footer();
 
