b832e8f5ced1e1385f793c4cde19ebe3c158112b
Grayson A.C. Laramore <info@sillycoyote.com>
2016-06-13 20:42:11 -0700

    switched to $output rendering style

--- footer.php
+++ footer.php
@@ -1,27 +1,24 @@
 <footer id="footer"><?php
-	echo '<div class="page-wrapper">';
 
 
-		/*echo '<div class="footer-content">';
-			if ( is_active_sidebar( 'first-footer-widget-area'  ) ) : ?><ul class="nobull footsidebar-1 one_fourth"><?php dynamic_sidebar( 'first-footer-widget-area' ); ?></ul><?php endif;
-			if ( is_active_sidebar( 'second-footer-widget-area' ) ) : ?><ul class="nobull footsidebar-2 one_fourth"><?php dynamic_sidebar( 'second-footer-widget-area' ); ?></ul><?php endif;
-			if ( is_active_sidebar( 'third-footer-widget-area'  ) ) : ?><ul class="nobull footsidebar-3 one_fourth"><?php dynamic_sidebar( 'third-footer-widget-area' ); ?></ul><?php endif;
-			if ( is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?><ul class="nobull footsidebar-4 one_fourth last"><?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?></ul><?php endif;
-		echo '<div class="clear"></div>'; //*/
+
+if ( $date > $start_year ) $date = "$start_year - $date"; // If the two years don't match, they go to 'n fro. If you don't want the hyphenated date, comment out this line.
+
+	$output .= '<div class="page-wrapper">';
 
 
-		echo '<div role="contentinfo" id="footer-copyright" class="textcenter">';
-			$start_year = "2015"; // Starting Year
+		// Copyright date
+			$start_year = "2016"; // Starting Year
 			$date = date( 'Y' ); // Current year
-			if ( $date > $start_year ) $date = "$start_year - $date"; // If the two years don't match, they go to 'n fro. If you don't want the hyphenated date, comment out this line.
+			$output .= '<div role="contentinfo" id="footer-copyright" class="textcenter">';
+				$output .= __( sprintf( 'Copyright &copy; %1$s <a href="%2$s" title="%3$s" rel="home">%4$s</a>. All Rights Reserved. ' , $date, home_url('/'), esc_attr( get_bloginfo( 'name', 'display' ) ), get_bloginfo( 'name' )), 'skivvy' );
+				$output .= __( '<a href="'. skinfo('AuthorURI') . '" title="" rel="nofollow" target="_blank">Web Design</a> by '. skinfo('Author') , 'skivvy' );
+			$output .= '</div>';
 
-			_e( sprintf( 'Copyright &copy; %1$s <a href="%2$s" title="%3$s" rel="home">%4$s</a>. All Rights Reserved. ' , $date, home_url('/'), esc_attr( get_bloginfo( 'name', 'display' ) ), get_bloginfo( 'name' )), 'skivvy' );
+	$output .= '</div>';
+		
 
-			if (function_exists('skinfo')) { _e( '<a href="'. skinfo('AuthorURI') . '" title="" rel="nofollow" target="_blank">Web Design</a> by '. skinfo('Author') , 'skivvy' ) ; }
-		echo '</div>';
-
-
-	echo '</div>';
+	echo $output;
 	wp_footer();
 ?></footer>
 </body>
