9a4bd0fe383f177e0eb89779838367251a18d791
Coyoli <info@sillycoyote.com>
2017-08-31 20:51:44 -0700

    Fixed footer bugs with wp_foot & copyright date

--- footer.php
+++ footer.php
@@ -1,25 +1,28 @@
-<footer id="footer"><?php
+<?php
+
+	$output .= '<footer id="footer">';
+
+		$output .= '<div class="page-wrapper">';
 
 
+			// Copyright date
+				$start_year = "2017"; // Starting Year
+				$date = date( 'Y' ); // Current year
+				if ( $date > $start_year ) $date = "$start_year - $date";
 
-if ( $date > $start_year ) $date = "$start_year - $date"; // If the two years don't match, they go to 'n fro. If you don't want the hyphenated date, comment out this line.
+				$output .= '<div role="contentinfo" id="footer-copyright" class="textcenter">';
+					$output .= __( sprintf( 'Copyright &copy; %1$s <a href="%2$s" title="%3$s" rel="home">%4$s</a>.', $date, home_url('/'), esc_attr( get_bloginfo( 'name', 'display' ) ), get_bloginfo( 'name' )), 'skivvy' );
+					$output .= __( ' All Rights Reserved.', 'skivvy' );
+					$output .= __( ' <a href="'. skinfo('AuthorURI') . '" title="" rel="nofollow" target="_blank">Web Design</a> by '. skinfo('Author') , 'skivvy' );
+				$output .= '</div>';
 
-	$output .= '<div class="page-wrapper">';
+		$output .= '</div>';
 
-
-		// Copyright date
-			$start_year = "2016"; // Starting Year
-			$date = date( 'Y' ); // Current year
-			$output .= '<div role="contentinfo" id="footer-copyright" class="textcenter">';
-				$output .= __( sprintf( 'Copyright &copy; %1$s <a href="%2$s" title="%3$s" rel="home">%4$s</a>. All Rights Reserved. ' , $date, home_url('/'), esc_attr( get_bloginfo( 'name', 'display' ) ), get_bloginfo( 'name' )), 'skivvy' );
-				$output .= __( '<a href="'. skinfo('AuthorURI') . '" title="" rel="nofollow" target="_blank">Web Design</a> by '. skinfo('Author') , 'skivvy' );
-			$output .= '</div>';
-
-	$output .= '</div>';
-		
+	$output .= '</footer>';
 
 	echo $output;
-	wp_footer();
-?></footer>
+
+?>
+<?php wp_footer(); ?>
 </body>
 </html>
