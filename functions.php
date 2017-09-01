56e5b2e1d9dc4b6640ab711d491dd4acbf61cc4e
Coyoli <info@sillycoyote.com>
2017-08-31 20:46:43 -0700

    updated new location for admin

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
-<?php
+<?php # 2017-08-31
 	include 'lib/skivvy_func_auto.php';		// Auto Functions
 	include 'lib/skivvy_func_user.php';		// Usable Functions
 	include 'lib/skivvy_menu_render.php';	// Nav Menu functions
-	include 'lib/admin/index.php';			// Admin Functions & Styling
+	include 'lib/skivvy_admin.php';			// Admin Functions & Styling
 	include 'lib/skivvy_simple.php';		// Cleanup
 	include 'lib/skivvy_register.php';		// Registry
-?>
