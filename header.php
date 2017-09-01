0741ce1a2bbe296336bb5202fcdfd5ec7e6d4de3
Coyoli <info@sillycoyote.com>
2017-08-31 20:53:24 -0700

    restored full logo render

--- header.php
+++ header.php
@@ -3,35 +3,36 @@
 <head><?php wp_head(); ?></head>
 <body id="page-<?php the_ID(); ?>" <?php body_class(); ?>>
 <?php
-$output .= '<header role="banner" id="header">';
-	$output .= '<div class="page-wrapper">';
+
+	$output .= '<header role="banner" id="header">';
+		$output .= '<div class="page-wrapper">';
 
 			// Logo
-				$output .= get_skivvy_logo( array(
-					'class' => 'alignleft',
-				));
+				$output .= '<a id="logo" class="alignleft" href="' . home_url( '/' ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" rel="home">';
+					$output .= '<img src="' . get_stylesheet_directory_uri() . '/img/logo.png" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '">';
+				$output .= '</a>';
 
 			// Mobile Toggle
 				$output .= '<button id="mobile-toggle" class="alignright dont-print"></button>';
 
-			// Main Menu
 				$output .= '<nav role="navigation" class="dont-print">';
-					$output .= wp_nav_menu( array(
+
+					// Main Menu
+						$output .= wp_nav_menu( array(
 							'echo'            => false,
 							'container'       => false,
 							'theme_location'  => 'main',
 							'menu_id'         => 'main-menu',
-							'menu_class'      => 'clearfix nobull dropdown animated flyoutleft justified', // Menu functionality classes
+							'menu_class'      => 'clearfix nobull dropdown animated flyout-right justified', // Menu functionality classes
 							'fallback_cb'     => false,
 							'depth'           => 3, // 0 = all. Default, -1 = displays links at any depth and arranges them in a single, flat list.
 							'walker'          => new skivvy_walker_main,
-
-					));
+						));
 
 					$output .= '<div class="clear"></div>';
 
 					// Mobile Menu
-					$output .= wp_nav_menu( array(
+						$output .= wp_nav_menu( array(
 							'echo'            => false,
 							'container'       => false,
 							'theme_location'  => 'mobile',
@@ -39,11 +40,12 @@ $output .= '<header role="banner" id="header">';
 							'menu_class'      => 'nobull textcenter clearfix',
 							'items_wrap'      => '<ul style="display:none;" id="%1$s" class="%2$s">%3$s</ul>',
 							'depth'           => -1,
-					));
+						));
+
 				$output .= '</nav>';
 
 
-	$output .= '</div>';
-$output .= '</header>';
+		$output .= '</div>';
+	$output .= '</header>';
+
 echo $output;
-?>
