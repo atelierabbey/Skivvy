03da6283f282ddd35d5c512355ffe7a3c97fb78b
Coyoli <info@sillycoyote.com>
2017-09-05 22:09:51 -0700

    postmeta now renders with $output

--- inc/chunk-postmeta.php
+++ inc/chunk-postmeta.php
@@ -1,69 +1,62 @@
-<div class="post-meta"><?php
+<?php
+
+$output = '<div class="post-meta">';
+
+ 	if ( is_singular() ) {
+
+		// POST AUTHOR
+			$output .= 'By ';
+				$output .= '<a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" title="View all posts by ' . get_the_author() . '">';
+					$output .= get_the_author();
+				$output .= '</a> ';
+		//*/
 
 
-	// POST AUTHOR
+		// POST DATE
+			$output .= ' | Published ';
+				$output .= '<abbr title="' . esc_attr( get_the_date() . ' ' . get_the_time() ) . '">';
+					$output .= get_the_time(get_option('date_format'));
+				$output .= '</abbr> ';
+
+		// POST CATEGORIES
+			$cats_list = get_the_category_list( ', ' );
+			if ( $cats_list ) {
+				$output .= ' | Category: ' . $cats_list;
+			}
+		
+		// POST TAGS
+			$tags_list = get_the_tag_list( '', ', ' );
+			if ( $tags_list ) {
+				$output .= ' | Tags: ' . $tags_list;
+			}
 	
-		/* printf(__('By %2$s ', 'skivvy'),
-			'meta-prep meta-prep-author',
-			sprintf( '<a class="url fn n" href="%1$s" title="%2$s">%3$s</a>',
-				get_author_posts_url( get_the_author_meta( 'ID' ) ),
-				sprintf( esc_attr__( 'View all posts by %s', 'skivvy' ), get_the_author() ),
-				get_the_author()
-				)
-		); //*/
 
-
-	// POST DATE
+	} elseif ( is_category() || is_tag() || is_tax() ) {
+		// Category, tag, and taxonomy
 	
-		the_time(get_option('date_format'));
-		/* printf( __('Published %2$s', 'skivvy'),
-				'meta-prep meta-prep-entry-date',
-				sprintf( '<abbr title="%1$s">%2$s</abbr>',
-							esc_attr( get_the_time() ),
-							get_the_date()
-						)
-		); //*/
-
-	// POST CATEGORIES
-		$cats_list = get_the_category_list( ', ' );
-			if ( $cats_list ) echo __( ' | Category:' , 'skivvy' ). ' ' . $cats_list;
-
+			$output .= '<div id="term_description">' . term_description() . '</div>';
 	
-	// POST TAGS
-		$tags_list = get_the_tag_list( '', ', ' );
-			if ( $tags_list ) echo __( ' | Tags:' , 'skivvy' ) . ' ' . $tags_list;
+	} elseif ( is_search() ) {
+		// Search
+			// Or if search, show Result Search Count
+				$allsearch = &new WP_Query("s=$s&showposts=-1");
+				$key = wp_specialchars($s, 1);
+				$count = $allsearch->post_count;
+				$output .= '<h2>Showing ' . $count . '  results for: ' . $key . '</h2>';
+				wp_reset_postdata();
+			//*/
 	
-	// Category, tag, and taxonomy
-	if ( is_category() || is_tag() || is_tax() ) :
-			term_description();
-	/*
-	// Search
-	elseif ( is_search() ) :
-		// Or if search, show Result Search Count
-			$allsearch = &new WP_Query("s=$s&showposts=-1");
-			$key = wp_specialchars($s, 1);
-			$count = $allsearch->post_count;
-			printf( __( '<h2>Showing %1$s  results for: %2$s</h2>', 'skivvy' ), $count , $key );
-			wp_reset_query(); 
-	//*/
-
-
-	// Attachment
-	elseif ( is_attachment() ) :
+	
+	} elseif ( is_attachment() ) {
+		// Attachment
 
 		if ( wp_attachment_is_image() ) {
-
 			$metadata = wp_get_attachment_metadata();
-
-			printf( __( ' | Full size is %s pixels', 'skivvy'),
-				sprintf( '<a href="%1$s" title="%2$s">%3$s &times; %4$s</a>',
-					wp_get_attachment_url(),
-					esc_attr( __('Link to full-size image', 'skivvy') ),
-					$metadata['width'],
-					$metadata['height']
-				)
-			);
+			$output .= ' | Full size is <a href="'. wp_get_attachment_url() . '" title="' . esc_attr( 'Link to full-size image' ) . '">' . $metadata['width'] . ' &times; ' . $metadata['height'] . '</a> pixels';
 		}
 
+	}
 
-endif; ?></div>
+$output .= '</div>';
+
+echo $output;
