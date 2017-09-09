dd39422b2fadd21096b9cd9c27be2f00b25070a9
SillyCoyote <SillyCoyote@users.noreply.github.com>
2017-09-09 13:05:12 -0700

    Updated #mobile-nav to #mobile-menu

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 
-	$('#mobile-nav').slideUp(0);$('#mobile-toggle').click(function(){$("#mobile-nav").slideToggle(400);});// MOBILE TOGGLE
-	$('#mobile-nav .menu-item-has-children').addClass('menu-closed').children('ul').slideUp(0);$('#mobile-nav .menu-item-has-children').click(function(){$(this).toggleClass('menu-closed').children('ul').slideToggle(400);});// MOBILE MENU ACCORDION
+	$('#mobile-menu').slideUp(0);$('#mobile-toggle').click(function(){$("#mobile-menu").slideToggle(400);});// MOBILE TOGGLE
+	$('#mobile-menu .menu-item-has-children').addClass('menu-closed').children('ul').slideUp(0);$('#mobile-menu .menu-item-has-children').click(function(){$(this).toggleClass('menu-closed').children('ul').slideToggle(400);});// MOBILE MENU ACCORDION
 
 });
